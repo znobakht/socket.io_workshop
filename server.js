9f77b7405bc63014a66ef72d4b78a9ed8f626eeb
zakie nobakht <zakie.nobakht@gmail.com>
2022-06-09 20:48:58 +0430

    server.js refactored

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
-const http = require("http");
-const socket_Io = require("socket.io");
 const express = require("express");
+const app = express();
+
 const mongoose = require("mongoose");
 
-const app = express();
+const http = require("http");
 const server = http.createServer(app);
-const IO = socket_Io(server);
 
-const chat = require("./routes/chat")(IO);
+const io = require("socket.io")(server);
+
+const chat = require("./routes/chat")(io);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
 const port = process.env.PORT || 5000;
-server.listen(port, ()=>console.log(`server running on port ${port}`));
+server.listen(port, () => console.log(`server running on port ${port}`));
 
 const db = require("./config/keys.json").mongoURI;
-
-mongoose.connect(db)
-.then(()=>console.log("connected to database"))
-.catch(err => console.error(err));
+mongoose
+  .connect(db)
+  .then(() => console.log("connected to database"))
+  .catch((err) => console.error(err));
