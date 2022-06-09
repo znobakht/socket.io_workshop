b30de8427eae1bf1548056890375bf25a2d31ca1
zakie nobakht <zakie.nobakht@gmail.com>
2022-06-09 19:05:33 +0430

    mongodb initiation

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const socket_Io = require("socket.io");
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 const server = http.createServer(app);
@@ -10,3 +11,9 @@ const chat = require("./routes/chat")(IO);
 
 const port = process.env.PORT || 5000;
 server.listen(port, ()=>console.log(`server running on port ${port}`));
+
+const db = require("./config/keys.json").mongoURI;
+
+mongoose.connect(db)
+.then(()=>console.log("connected to database"))
+.catch(err => console.error(err));
