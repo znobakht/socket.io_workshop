7546d798e6b644ff2851b2ca31513f235b34286b
zakie nobakht <zakie.nobakht@gmail.com>
2022-06-10 12:48:31 +0430

    broadcasting a message to other user when a new user connected

--- routes/chat.socket.js
+++ routes/chat.socket.js
@@ -1,7 +1,9 @@
 module.exports = async function(io){
     io.on('connection', (socket) => {
         console.log('a new user connected');
-        socket.emit('message', 'welcome');
+        socket.emit("welcomeMessage", "welcome");
+        socket.broadcast.emit("welcomeMessage", "a new user connected to server");
+        
         socket.on("message", (msg) => {
           io.emit("message", msg);
         });
